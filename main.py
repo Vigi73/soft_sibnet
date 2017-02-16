d0d06178a574e31ad3b65304770984dba75d1596
vigi73 <05071973kur>
2017-02-16 14:54:04 +0700

    Старт проекта

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from my_lib import get_html
+from bs4 import BeautifulSoup as BS
+
+
+
+
 
 
 def main():
+    url = f'http://soft.sibnet.ru/search/?text=*&os=*&&pg=1'
     pass
-
-
-
 
 
 if __name__ == '__main__':
